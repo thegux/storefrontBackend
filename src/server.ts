17d9a13f3c3f4fbd19f7597c8f4ca6f7df2107f0
Gabriel GUX <gabrielsrbezerra@gmail.com>
2022-02-17 21:01:39 -0300

    Added cors to the application

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes';
+import cors from 'cors';
 
 const app: express.Application = express();
 const address = 'localhost:3000';
 
+const corsOptions = {
+	origin: 'https://localhost:3000',
+	optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.use('/', routes);
